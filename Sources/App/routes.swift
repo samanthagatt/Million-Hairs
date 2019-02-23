b8d26f465852411d67c99ba1ebf7bc948854200a
Samantha Gatt <smfgatt@gmail.com>
2019-02-23 12:12:09 -0500

    Adds a staff page with links to other members

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -16,8 +16,34 @@ public func routes(_ router: Router) throws {
         return try req.view().render("contact", context)
     }
     
-//    router.get("staff") { req -> Future<View> in
-//        let context: [String: String] = [:]
-//        return try req.view().render("staff", context)
-//    }
+    router.get("staff", String.parameter) { req -> Future<View> in
+        let name = try req.parameters.next(String.self)
+        
+        let context: StaffView
+        
+        if let bio = StaffView.bios[name] {
+            context = StaffView(name: name, bio: bio)
+        } else {
+            context = StaffView()
+        }
+        
+        return try req.view().render("staff", context)
+    }
+}
+
+
+
+
+struct StaffView: Codable {
+    var name: String?
+    var bio: String?
+    let allBios = StaffView.bios.keys.sorted()
+    
+    static let bios = [
+        "kirk": "My name is James Kirk and I love snakes",
+        "picard": "My name is Jean-Luc Picard and I'm mad for fish.",
+        "sisko": "My name is Benjamin Sisko and I'm all about the budgies.",
+        "janeway": "My name is Kathryn Janeway and I want to hug every hamster.",
+        "archer": "My name is Jonathan Archer and beagles are my thing."
+    ]
 }
